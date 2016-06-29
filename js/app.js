30941c0e859ec6497fa18105ece3dd8a8976ad13
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-06-29 15:47:44 -0600

    frist push

---
+++ js/app.js
@@ -0,0 +1,43 @@
+var Model = function() {
+    this.text = "";
+    console.log(this.text);
+    this.onChange = null;
+};
+
+Model.prototype.setText = function(value) {
+    this.text = value.toUpperCase();
+    if (this.onChange) {
+        this.onChange(this.text);
+    }
+};
+
+var View = function(elementSelector, initialValue) {
+    this.element = $(elementSelector);
+    this.setValue(initialValue || '');
+    this.onChange = null;
+
+    this.element.on('input', this.onInput.bind(this));
+};
+
+View.prototype.onInput = function() {
+    var value = this.element.val();
+    if (this.onChange) {
+        this.onChange(value);
+    }
+};
+
+View.prototype.setValue = function(text) {
+    this.element.val(text);
+};
+
+var Controller = function(model, view) {
+    view.onChange = model.setText.bind(model);
+    model.onChange = view.setValue.bind(view);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    var model = new Model();
+    var view = new View('.uppercase');
+    var controller = new Controller(model, view);
+
+});
